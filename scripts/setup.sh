125c55f17e6ccb13ba24e4b1f117ab55fd63a03d
KSBilodeau <22333708+KSBilodeau@users.noreply.github.com>
2023-12-06 06:17:38 +0000

    Initial commit

---
+++ scripts/setup.sh
@@ -0,0 +1,154 @@
+#!/usr/bin/env bash
+
+check_installation() {
+	if rpm -q $1 >/dev/null 2>&1 ; then
+		echo "$1 is installed"
+	else
+		echo "Ansible not found; Installing..."
+
+		if sudo yum install $1 -qy >/dev/null 2>&1 ; then
+			echo "$1 is installed"
+		else
+			echo "ABORT: $1 failed to install"
+			exit 1
+		fi
+	fi
+}
+
+log() {
+	echo -e $1
+	echo -e "$1" | sudo tee -a setup.log >/dev/null 2>&1
+}
+
+log_no_print() {
+	echo -e "$1" | sudo tee -a setup.log >/dev/null 2>&1
+}
+
+# Mark date of execution in log
+
+log "DATE OF EXECUTION: $(date '+%Y-%m-%d %H:%M:%S')"
+
+# Check for necessary packages
+
+log "Checking for necessary packages:\n"
+
+check_installation ansible
+check_installation git
+
+log "\nAll packages installed!\n"
+
+# Get the type of server we're configuring (test/dev/prod)
+
+while true ; do
+	read -p "Enter server type (test/dev/prod): " strServerType
+
+	if [ $strServerType = "test" ] || [ $strServerType = "dev" ] || [ $strServerType = "prod" ] ; then
+		break
+	fi  
+
+	log "Invalid Response: Try again"
+done
+
+log "Server type acquired: $strServerType\n"
+
+log "Checking for prior system configuration:"
+
+# Check if there are any IPs set in the ansible host file
+# (meaning this script has been run before)
+
+if sudo grep -qFx "[web$strServerType]" /etc/ansible/hosts >/dev/null 2>&1 ; then
+	log "Ansible configuration found!"
+else
+	# Configure ansible with my defaults for test/prod/dev
+
+	log "Ansible configuration not found!\nConfiguring ansible...\n"
+
+	# Disable host key checking so that connections can be automated completely
+
+	if grep -Fxq "#host_key_checking = False" /etc/ansible/ansible.cfg >/dev/null 2>&1 ; then
+		sudo sed -i "s/#host_key_checking = False/host_key_checking = False/" /etc/ansible/ansible.cfg
+		log_no_print "Disabled host key checking..."
+	fi
+
+	# Check if an SSH key is present
+
+	if [ -f ~/.ssh/id_ed25519 ] ; then
+		log_no_print "SSH key already present"
+	else
+		log_no_print "Generating SSH Key..."
+
+		if yes "" | ssh-keygen -t ed25519 -N "" >/dev/null 2>&1 ; then
+			eval "$(ssh-agent -s)" >/dev/null 2>&1
+			ssh-add ~/.ssh/id_ed25519 >/dev/null 2>&1
+
+			log_no_print "SSH key generated and added"
+		else
+			log_no_print "Failed to generate key"
+			exit 2
+		fi
+	fi
+
+	# Get the IPs that are to be managed
+
+	read -p "Enter the internal IPs to be managed as a space separated list: " strIpAddrs
+
+	log "\nIPs to apply to hosts file:"
+
+	# Convert the IP string into an array
+
+	arrIpAddrs=( $strIpAddrs )
+
+	# Prep the hosts file to have the IP addrs appended
+
+	echo -e "\n[web$strServerType]\n" | sudo tee -a /etc/ansible/hosts >/dev/null 2>&1
+
+	i=1
+	for ip in ${arrIpAddrs[@]} ; do
+		log $ip
+
+		# Append each IP address to the host file
+
+		echo "root@$ip" | sudo tee -a /etc/ansible/hosts >/dev/null 2>&1
+	done
+
+	# Copy the keys to the servers
+
+	log "\nPlease enter root passwd for each server: (IF NECESSARY)\n"
+
+	for ip in ${arrIpAddrs[@]} ; do
+		ssh-copy-id -i ~/.ssh/id_ed25519 root@$ip
+	done
+fi
+
+log "\nRunning ansible test run..."
+
+if ansible all -m ping ; then 
+	log "Ansible ran successfully!"
+else
+	log "Ansible failed!"
+	exit 3
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
